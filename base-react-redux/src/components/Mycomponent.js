5a8b58c2b95e53a77cc71ea2c13fe1348daab8bb
Cong Nghia IT <nghia2801dt@gmail.com>
2024-07-03 13:25:42 +0700

    Video21_nghia

--- base-react-redux/src/components/Mycomponent.js
+++ base-react-redux/src/components/Mycomponent.js
@@ -10,12 +10,35 @@ class Mycomponent extends React.Component {
       name: "hù hu hú",
     });
   };
+  handleOnChangeInput = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+
+    // alert("hello");
+  };
 
   render() {
     return (
       <div>
         <h2>heloo</h2>;<h2>what's your name {this.state.name}</h2>
         <button onClick={this.handleClick}>Click here</button>
+        <form
+          onSubmit={(event) => {
+            this.handleOnSubmit(event);
+          }}
+        >
+          <input
+            type="text"
+            onChange={(event) => {
+              this.handleOnChangeInput(event);
+            }}
+          ></input>
+          <button>submit</button>
+        </form>
       </div>
     );
   }
