dcff0013c766b8e8dcbf4c01f12678296dcf25fc
Cong Nghia IT <nghia2801dt@gmail.com>
2024-07-03 11:49:01 +0700

    Nghia-Commit-20-setStage

--- base-react-redux/src/components/Mycomponent.js
+++ base-react-redux/src/components/Mycomponent.js
@@ -4,10 +4,18 @@ class Mycomponent extends React.Component {
     name: "hoa hóa hòa",
     age: 20,
   };
+
+  handleClick = () => {
+    this.setState({
+      name: "hù hu hú",
+    });
+  };
+
   render() {
     return (
       <div>
         <h2>heloo</h2>;<h2>what's your name {this.state.name}</h2>
+        <button onClick={this.handleClick}>Click here</button>
       </div>
     );
   }
