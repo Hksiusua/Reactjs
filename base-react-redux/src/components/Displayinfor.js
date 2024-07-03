539daf2d08c300519398a76a0b5d846d4f0b047b
Cong Nghia IT <nghia2801dt@gmail.com>
2024-07-03 15:35:48 +0700

    25-Nghia-hideshow

--- base-react-redux/src/components/Displayinfor.js
+++ base-react-redux/src/components/Displayinfor.js
@@ -1,19 +1,44 @@
 import React from "react";
 
 class Displayinfor extends React.Component {
+  state = {
+    isshowListUser: true,
+  };
+  handleShowHide = () => {
+    this.setState({
+      isshowListUser: !this.state.isshowListUser,
+    });
+  };
+
   render() {
     const { listUsers } = this.props;
 
     return (
       <div>
-        {listUsers.map((user) => {
-          return (
-            <div key={user.id}>
-              <div>My name {user.name}</div>
-              <div>My age {user.age}</div>
-            </div>
-          );
-        })}
+        <div>
+          <span
+            onClick={() => {
+              this.handleShowHide();
+            }}
+          >
+            Hide list users <br></br>
+            {this.state.isshowListUser === true
+              ? "Hide list users"
+              : "show list users"}
+          </span>
+        </div>
+        {this.state.isshowListUser && (
+          <div>
+            {listUsers.map((user) => {
+              return (
+                <div key={user.id} className={user.age > 20 ? "red" : "green"}>
+                  <div>My name {user.name}</div>
+                  <div>My age {user.age}</div>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     );
   }
