10a0612bb0ca8b15aaec66f368fa19e51c22b002
Cong Nghia IT <nghia2801dt@gmail.com>
2024-07-03 10:58:09 +0700

    nghia-17

--- base-react-redux/src/components/Mycomponent.js
+++ base-react-redux/src/components/Mycomponent.js
@@ -1,7 +1,15 @@
 import React from "react";
 class Mycomponent extends React.Component {
+  state = {
+    name: "hoa hóa hòa",
+    age: 20,
+  };
   render() {
-    return <h2>heloo</h2>;
+    return (
+      <div>
+        <h2>heloo</h2>;<h2>what's your name {this.state.name}</h2>
+      </div>
+    );
   }
 }
 export default Mycomponent;
